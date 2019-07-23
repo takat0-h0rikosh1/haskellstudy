23d1cddf14e32a6646f7a62af3512d4e8be2b001
堀越貴斗 <t_horikoshi@septeni-original.co.jp>
2019-07-23 21:43:39 +0900

    add baby.hs

---
+++ baby.hs
@@ -0,0 +1,8 @@
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+addThree x y z = x + y + z 
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+circumference :: Float  -> Float
+circumference r = 2 * pi * r
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
