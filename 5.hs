331e45c46b3c0d09b0ffa8d2807f1066e3c824bf
takato.horikoshi <takato.h0rikosh1@gmail.com>
2019-08-02 01:30:52 +0900

    第5章 勉強中

---
+++ 5.hs
@@ -0,0 +1,11 @@
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+{-
+compareWithHundred :: Int -> Ordering
+compareWithHundred x = compare 100 x
+-}
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred = compare 100
+
