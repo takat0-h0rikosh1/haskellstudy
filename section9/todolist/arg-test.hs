52dc5c1f066605ef048d7cbe9414e869670ce7cd
takato.horikoshi <takato.h0rikosh1@gmail.com>
2019-10-15 00:21:01 +0900

    section9

---
+++ section9/todolist/arg-test.hs
@@ -0,0 +1,10 @@
+import System.Environment
+import Data.List
+
+main = do
+    args <- getArgs
+    progName <- getProgName
+    putStrLn "The argument are:"
+    mapM putStrLn args
+    putStrLn "The program name is:"
+    putStrLn progName
